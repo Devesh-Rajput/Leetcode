033c1319f9c52c02c01b547c3724264c08a473fa
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2025-10-29 10:00:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (89.59%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int k=1;
+        while(true){
+            int num=(1<<k)-1;
+            if(num >= n){
+                return num;
+            }
+            k++;
+        }
+    }
+};
