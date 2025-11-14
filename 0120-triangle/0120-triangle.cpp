3ad44fec79e348156d15e254b2a0b31fd34f3f2b
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2025-11-14 15:29:27 +0530

    Time: 0 ms (100%), Space: 13.5 MB (6.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,23 @@
 class Solution {
-    int helper(int i, int j, int &m, int &n, vector<vector<int>>& triangle, vector<vector<int>> &dp){
-        if(i==m) return 0;
+    // int helper(int i, int j, int &m, int &n, vector<vector<int>>& triangle, vector<vector<int>> &dp){
+    //     if(i==m) return 0;
 
-        if(dp[i][j] != INT_MIN) return dp[i][j];
+    //     if(dp[i][j] != INT_MIN) return dp[i][j];
 
-        int down=triangle[i][j] + helper(i+1, j, m, n, triangle, dp);
-        int downRight=triangle[i][j] + helper(i+1, j+1, m, n, triangle, dp);
+    //     int down=triangle[i][j] + helper(i+1, j, m, n, triangle, dp);
+    //     int downRight=triangle[i][j] + helper(i+1, j+1, m, n, triangle, dp);
 
-        return dp[i][j] = min(down, downRight);
-    }
+    //     return dp[i][j] = min(down, downRight);
+    // }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
-        int n=triangle[m-1].size();
-        vector<vector<int>> dp(m, vector<int>(n,INT_MIN));
-        return helper(0, 0, m, n, triangle, dp);
+        vector<vector<int>> dp= triangle;
+        for(int i=m-2; i>=0; i--){
+            for(int j=0; j<triangle[i].size(); j++){
+                dp[i][j]=triangle[i][j]+min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
