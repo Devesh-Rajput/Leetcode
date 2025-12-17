a1d64913877921fd658325ed4db70a0cba3bfbed
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2025-12-17 10:16:39 +0530

    Time: 0 ms (100%), Space: 8 MB (76.99%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int capitalCount=0;
+        for(char ch: word){
+            if(isupper(ch)) capitalCount++;
+        }
+
+        if(capitalCount==0 || capitalCount==word.size() || (capitalCount==1 && isupper(word[0]))) return true;
+        return false;
+    }
+};
