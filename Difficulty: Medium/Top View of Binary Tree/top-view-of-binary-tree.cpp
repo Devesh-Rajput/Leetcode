9e4a1ab4682336c8ddf5ee98f96e82e51b62da90
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2025-12-26 21:34:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -22,39 +22,28 @@ class Solution {
         find(root->left, l, r, pos-1);
         find(root->right, l, r, pos+1);
     }
+    
+    void tView(Node* root, int pos, vector<int> &ans, vector<int> &level, int lvl){
+        if(root==NULL) return;
+        
+        if(level[pos]>lvl){
+            ans[pos]=root->data;
+            level[pos]=lvl;
+        }
+        
+        tView(root->left, pos-1, ans, level, lvl+1);
+        tView(root->right, pos+1, ans, level, lvl+1);
+    }
+    
   public:
     vector<int> topView(Node *root) {
         // code here
         int l=0, r=0;
         find(root, l, r, 0);
         vector<int> ans(r-l+1);
-        vector<bool> filled(r-l+1, 0);
-        queue<Node*> q;
-        queue<int> index;
+        vector<int> level(r-l+1, INT_MAX);
         
-        q.push(root);
-        index.push(-l);
-        
-        while(!q.empty()){
-            Node* node=q.front();
-            q.pop();
-            int i=index.front();
-            index.pop();
-            
-            if(!filled[i]){
-                filled[i]=1;
-                ans[i]=node->data;
-            }
-            
-            if(node->left){
-                q.push(node->left);
-                index.push(i-1);
-            }
-            if(node->right){
-                q.push(node->right);
-                index.push(i+1);
-            }
-        }
+        tView(root, -1*l, ans, level, 0);
         return ans;
         
     }
