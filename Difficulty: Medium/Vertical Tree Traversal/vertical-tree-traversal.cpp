12463d0044aa5073a3b114332a45bc07d1229611
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2026-01-21 22:49:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.cpp
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.cpp
@@ -1,59 +1,72 @@
-/*
-class Node {
-  public:
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
-        data = val;
-        left = NULL;
-        right = NULL;
-    }
-};
-*/
-
 class Solution {
-  public:
+    void find(Node* root, int pos, int& l, int& r) {
+        if (root == NULL) return;
+        l = min(l, pos);
+        r = max(r, pos);
+        find(root->left, pos - 1, l, r);
+        find(root->right, pos + 1, l, r);
+    }
+
+public:
     vector<vector<int>> verticalOrder(Node *root) {
-        // code here
-        map<int, map<int, vector<int>>> nodes;
-        queue<pair<Node*, pair<int, int>>> q;
-        vector<vector<int>> ans;
-        
-        if(root==NULL)
-            return ans;
-            
-        q.push({root, {0, 0}});
-            
-        while(!q.empty()){
-            pair<Node*, pair<int, int>> temp=q.front();
-            q.pop();
-            
-            Node* frontNode=temp.first;
-            int hd=temp.second.first;
-            int lvl=temp.second.second;
-            
-            nodes[hd][lvl].push_back(frontNode->data);
-            
-            if(frontNode->left){
-                q.push(make_pair(frontNode->left,make_pair(hd-1, lvl+1)));
-            }
-            if(frontNode->right){
-                q.push(make_pair(frontNode->right,make_pair(hd+1, lvl+1)));
-            }
-        }
-        for (auto i : nodes) {
-            vector<int> col;                
-            for (auto j : i.second) {        
-                for (auto k : j.second) {    
-                    col.push_back(k);
+
+        int l = 0, r = 0;
+        find(root, 0, l, r);
+
+        vector<vector<int>> positive(r + 1);
+        vector<vector<int>> negative(abs(l) + 1);
+
+        queue<Node*> q;
+        queue<int> index;
+
+        q.push(root);
+        index.push(0);
+
+        while (!q.empty()) {
+
+            int sz = q.size();
+            map<int, vector<int>> level;  // col -> values at this level
+
+            while (sz--) {
+                Node* node = q.front();
+                q.pop();
+                int pos = index.front();
+                index.pop();
+
+                level[pos].push_back(node->data);
+
+                if (node->left) {
+                    q.push(node->left);
+                    index.push(pos - 1);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                    index.push(pos + 1);
                 }
             }
-            ans.push_back(col);              
+
+            // ✅ push in column order, but keep left-to-right order inside vector
+            for (auto &it : level) {
+                int pos = it.first;
+                auto &vals = it.second;
+
+                if (pos >= 0) {
+                    for (int x : vals) positive[pos].push_back(x);
+                } else {
+                    for (int x : vals) negative[abs(pos)].push_back(x);
+                }
+            }
         }
 
-        
+        vector<vector<int>> ans;
+
+        for (int i = (int)negative.size() - 1; i > 0; i--) {
+            ans.push_back(negative[i]);
+        }
+        for (int i = 0; i < (int)positive.size(); i++) {
+            ans.push_back(positive[i]);
+        }
+
         return ans;
     }
 };
