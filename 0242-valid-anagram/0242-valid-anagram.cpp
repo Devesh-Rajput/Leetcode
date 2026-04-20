5cb24a3b50b092a22a138d5d3c478af397c9dc21
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2026-04-20 22:04:01 +0530

    Time: 0 ms (100%), Space: 9.8 MB (23.92%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return 0;
+
+        vector<int> freq(26,0);
+        for(char ch: s){
+            freq[ch-'a']++;
+        }
+        for(char ch: t){
+            freq[ch-'a']--;
+        }
+        for(int cnt:freq){
+            if(cnt!=0) return 0;
+        }
+        return 1;
+    }
+};
