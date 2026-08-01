9fb396c1f24ec71078c904769ca6aa2447e909c2
Devesh Kumar <166695849+Devesh-Rajput@users.noreply.github.com>
2026-08-01 10:55:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    bool helper(int p1, int p2, vector<int>& nums, int l, int r, bool player) {
+        if (l > r)
+            return p1 >= p2;
+
+        if (player) {
+            return helper(p1 + nums[l], p2, nums, l + 1, r, false) ||
+                   helper(p1 + nums[r], p2, nums, l, r - 1, false);
+        } else {
+            return helper(p1, p2 + nums[l], nums, l + 1, r, true) ||
+                   helper(p1, p2 + nums[r], nums, l, r - 1, true);
+        }
+    }
+
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        bool player = 1;
+        int n = nums.size();
+        return helper(0, 0, nums, 0, n - 1, player);
+    }
+};
